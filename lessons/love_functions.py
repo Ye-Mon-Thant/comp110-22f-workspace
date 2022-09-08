65c82413c86c4a9dfc81fdc98f7ec371b3aa1895
Ye Mon Thant <ythant@unc.edu>
2022-09-08 14:43:51 -0400

    9/8/2022

---
+++ lessons/love_functions.py
@@ -0,0 +1,18 @@
+"""Testing 1, 2, 3....."""
+
+def love(subject: str) -> str:
+    """Given a subject as a parameter, returns a loving string."""
+    return f"I love you {subject}!"
+
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a str repeating a loving message n times."""
+    love_note: str = ""
+    counter: int = 0
+    while counter <n:
+        love_note += love(to) + "\n"
+        counter += 1
+    return love_note 
+
+print(love("watermelon"))
+print(spread_love("watermelon", 5))
